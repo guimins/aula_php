d69364ed6972655a4938cd3002e618c38eb8ddda
guimins <3guicontact0@gmail.com>
2025-05-17 14:48:27 -0300

    treino de coisas ai

---
+++ aula.php
@@ -0,0 +1,34 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>nota</h1>
+   <?php
+  /* $nota = 9;
+   if ($nota < 5) {
+    echo 'aluno reprovado';
+   } else if ($nota < 7) {
+    echo 'aluno em recuperação';
+   }
+    else {
+    echo 'aluno aprovado';
+   }
+
+   */
+
+   function aguarde () {
+    echo 'aguarde um instante...';
+   }
+   aguarde();
+   aguarde();
+   aguarde();
+   aguarde();
+   aguarde();
+   aguarde();
+   ?>
+    
+</body>
+</html>
